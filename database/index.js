930d670e92e14f35923a0f8a465a863e91fc3a40
Alex <alexlance13@gmail.com>
2019-12-26 20:29:14 +0200

    Heroku

--- database/index.js
+++ database/index.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
-const models = require("./models");
+const mongoose = require('mongoose');
+const models = require('./models');
 
-mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
-db.on("error", () => console.log("some DB error"));
-db.once("open", () => {
-  console.log("DB connected");
+db.on('error', () => console.log('some DB error'));
+db.once('open', () => {
+  console.log('DB connected');
 });
 
 module.exports = {
   db,
-  models
+  models,
 };
--- index.js
+++ index.js
@@ -1,77 +1,77 @@
-require("dotenv").config();
-require("express-async-errors");
-const middlewares = require("./middlewares");
-const { db } = require("./database");
-const express = require("express");
-const session = require("express-session");
+require('dotenv').config();
+require('express-async-errors');
+const middlewares = require('./middlewares');
+const { db } = require('./database');
+const express = require('express');
+const session = require('express-session');
 const app = express();
-const controllers = require("./controllers");
-const cors = require("cors");
+const controllers = require('./controllers');
+const cors = require('cors');
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(
   session({
-    secret: "secret",
+    secret: 'secret',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
-  })
+    cookie: { secure: true },
+  }),
 );
 app.use(
   cors({
-    "Access-Control-Allow-Origin": "*"
-  })
+    'Access-Control-Allow-Origin': '*',
+  }),
 );
 app.use(middlewares.getUserFromHeader);
 
-db.once("open", () => {
-  app.listen(3333, () => {
-    console.log("Server ON");
-  });
+db.once('open', () => {
+  app.listen(process.env.PORT);
 });
 
 app
-  .route("/posts")
+  .route('/posts')
   .get(controllers.posts.getAll)
   .post([middlewares.loggedInCheck, controllers.posts.create])
   .delete([middlewares.loggedInCheck, middlewares.adminCheck, controllers.posts.remove]);
 
 app
-  .route("/users")
+  .route('/users')
   .get([middlewares.loggedInCheck, middlewares.adminCheck, controllers.users.getAll])
   .delete([middlewares.loggedInCheck, middlewares.adminCheck, controllers.users.remove]);
 
 app
-  .route("/users/:id")
+  .route('/users/:id')
   .get(controllers.users.getById)
   .delete([middlewares.loggedInCheck, middlewares.adminCheck, controllers.users.removeById]);
 
 app
-  .route("/posts/:id")
+  .route('/posts/:id')
   .get(controllers.posts.getById)
   .put([middlewares.loggedInCheck, controllers.posts.update])
   .delete([middlewares.loggedInCheck, controllers.posts.removeById]);
 
-app.route("/comments")
+app
+  .route('/comments')
   .post([middlewares.loggedInCheck, controllers.comments.create])
-  .get([middlewares.loggedInCheck, middlewares.adminCheck, controllers.comments.getAll])
+  .get([middlewares.loggedInCheck, middlewares.adminCheck, controllers.comments.getAll]);
 
-app.route("/comment/:id")
+app
+  .route('/comment/:id')
   .delete([middlewares.loggedInCheck, controllers.comments.remove])
-  .put([middlewares.loggedInCheck, middlewares.adminCheck, controllers.comments.approve])
+  .put([middlewares.loggedInCheck, middlewares.adminCheck, controllers.comments.approve]);
 
-app.post("/like", [middlewares.loggedInCheck, controllers.like.toggle]);
+app.post('/like', [middlewares.loggedInCheck, controllers.like.toggle]);
 
-app.post("/register", controllers.auth.register);
+app.post('/register', controllers.auth.register);
 
-app.post("/login", controllers.auth.login);
+app.post('/login', controllers.auth.login);
 
-app.post("/upload", [
+app.post('/upload', [
   middlewares.loggedInCheck,
   controllers.uploads.uploadArray,
   controllers.uploads.uploadFiles,
-  controllers.attachments.create
+  controllers.attachments.create,
 ]);
 
 app.use((req, res, next) => {
