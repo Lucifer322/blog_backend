72bc6576009a6ccd684f6421665c06c90bc5dc07
Alex Lance <alexlance@Alexs-MacBook-Air.local>
2019-12-26 21:39:11 +0200

    little changes for heroku

--- database/models/index.js
+++ database/models/index.js
@@ -1,13 +1,13 @@
-const comment = require("./Comment");
-const like = require("./Like");
-const post = require("./Post");
-const user = require("./User");
-const attachment = require("./Attachment");
+const like = require('./Like');
+const comment = require('./Comment');
+const post = require('./Post');
+const user = require('./User');
+const attachment = require('./Attachment');
 
 module.exports = {
   comment,
   like,
   post,
   user,
-  attachment
+  attachment,
 };
